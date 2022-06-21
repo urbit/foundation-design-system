2e317719a304b6b68ffd0d3d93d85261d5d34013
Matilde Park <m@park.computer>
2022-06-20 21:46:36 -0700

    Footer: key children

--- dist/components/layout/Footer.js
+++ dist/components/layout/Footer.js
@@ -21,7 +21,7 @@ function Footer(_ref) {
   var _data$, _data$2;
 
   var _ref$data = _ref.data,
-      data = _ref$data === void 0 ? [[], []] : _ref$data;
+      data = _ref$data === void 0 ? [[{}], [{}]] : _ref$data;
   return /*#__PURE__*/_react["default"].createElement("footer", {
     className: "bg-wall-100 mt-20 w-full flex justify-center"
   }, /*#__PURE__*/_react["default"].createElement(_SingleColumn["default"], null, /*#__PURE__*/_react["default"].createElement(_Section["default"], {
@@ -29,11 +29,13 @@ function Footer(_ref) {
     className: "flex flex-row flex-wrap"
   }, data === null || data === void 0 ? void 0 : (_data$ = data[0]) === null || _data$ === void 0 ? void 0 : _data$.map(function (section) {
     return /*#__PURE__*/_react["default"].createElement("div", {
+      key: section.title,
       className: "w-1/2 md:w-1/3 flex flex-col shrink"
     }, /*#__PURE__*/_react["default"].createElement("h4", {
       className: "mt-16 mb-8"
     }, section.title), section.links.map(function (link) {
       return /*#__PURE__*/_react["default"].createElement(_link["default"], {
+        key: link.href,
         href: link.href,
         passHref: true
       }, /*#__PURE__*/_react["default"].createElement("a", {
@@ -44,6 +46,7 @@ function Footer(_ref) {
     className: "flex flex-col md:flex-row"
   }, data === null || data === void 0 ? void 0 : (_data$2 = data[1]) === null || _data$2 === void 0 ? void 0 : _data$2.map(function (link) {
     return /*#__PURE__*/_react["default"].createElement("div", {
+      key: link.href,
       className: "md:w-1/3"
     }, /*#__PURE__*/_react["default"].createElement(_link["default"], {
       href: link.href,
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,18 +3,21 @@ import SingleColumn from "./SingleColumn";
 import Section from "./Section";
 import React from "react";
 
-export default function Footer({ data = [[], []] }) {
+export default function Footer({ data = [[{}], [{}]] }) {
   return (
     <footer className="bg-wall-100 mt-20 w-full flex justify-center">
       <SingleColumn>
         <Section short className="flex flex-row flex-wrap">
           {data?.[0]?.map((section) => {
             return (
-              <div className="w-1/2 md:w-1/3 flex flex-col shrink">
+              <div
+                key={section.title}
+                className="w-1/2 md:w-1/3 flex flex-col shrink"
+              >
                 <h4 className="mt-16 mb-8">{section.title}</h4>
                 {section.links.map((link) => {
                   return (
-                    <Link href={link.href} passHref>
+                    <Link key={link.href} href={link.href} passHref>
                       <a className="type-medium text-wall-500">{link.title}</a>
                     </Link>
                   );
@@ -26,7 +29,7 @@ export default function Footer({ data = [[], []] }) {
         <Section className="flex flex-col md:flex-row">
           {data?.[1]?.map((link) => {
             return (
-              <div className="md:w-1/3">
+              <div key={link.href} className="md:w-1/3">
                 <Link href={link.href} passHref>
                   <a className="type-medium text-wall-500">{link.title}</a>
                 </Link>

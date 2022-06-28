b551ef9b1230c392afa275d76c805d9b64aa17da
Matilde Park <m@park.computer>
2022-06-28 16:29:22 -0700

    tailwind: add antiwall classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
       "wall-500": "#5A564F",
       "wall-600": "#24221E",
       washedWall: "rgba(240, 239, 236, .8)", // for overlays
+      // AntiWall for darkmode
+      "antiwall-100": "#1C1E21",
+      "antiwall-200": "#1C1F24",
+      "antiwall-300": "#373B42",
+      "antiwall-400": "#6E737B",
+      "antiwall-500": "#A5A9B0",
+      "antiwall-600": "#DBDDE1",
       // Greens (Theme Primary Color)
       "green-100": "#E5F7F1",
       "green-200": "#BFEBDB",
@@ -61,7 +68,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      invert: ["dark"],
+    },
   },
   plugins: [require("@tailwindcss/aspect-ratio")],
 };

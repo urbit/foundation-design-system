cf4dfaf02cb7e4e949682066e2265e221c6b86d3
Matilde Park <m@park.computer>
2022-09-14 10:34:53 -0700

    Math: unwrap nested expressions

--- src/components/markdown/Math.js
+++ src/components/markdown/Math.js
@@ -2,7 +2,8 @@ import React from "react";
 import { MathComponent } from "mathjax-react"
 
 const Math = ({ block = false, children }) => {
-    return <MathComponent display={block} tex={String.raw`${children}`} />;
+    const exp = children?.join ? children.map(e => e?.props?.children ? e.props.children : e).join("") : children
+    return <MathComponent display={block} tex={String.raw`${exp}`} />;
 };
 
 export default Math;

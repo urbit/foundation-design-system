2cac3b82dfcdc8a1e6826c6f754f22e34ac95996
Matilde Park <m@park.computer>
2022-10-03 18:33:21 -0700

    Math: continue drilling down to child equations when paragraph separated

--- src/components/markdown/Math.js
+++ src/components/markdown/Math.js
@@ -2,7 +2,8 @@ import React from "react";
 import { MathComponent } from "mathjax-react"
 
 const Math = ({ block = false, children }) => {
-    const exp = children?.join ? children.map(e => e?.props?.children ? e.props.children : e).join("") : children
+    let exp = children?.join ? children.map(e => e?.props?.children ? e.props.children : e).join("") : children;
+    exp = exp?.props?.children ? exp.props.children.join("") : exp;
     return <MathComponent display={block} tex={String.raw`${exp}`} />;
 };
 

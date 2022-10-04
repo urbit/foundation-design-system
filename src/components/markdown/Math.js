8e2de8500a779cb2fe8a58ca4b55026a33ba6a46
Matilde Park <m@park.computer>
2022-10-04 13:16:59 -0700

    math: not all drilled expressions are arrays, batman

--- src/components/markdown/Math.js
+++ src/components/markdown/Math.js
@@ -3,7 +3,11 @@ import { MathComponent } from "mathjax-react"
 
 const Math = ({ block = false, children }) => {
     let exp = children?.join ? children.map(e => e?.props?.children ? e.props.children : e).join("") : children;
-    exp = exp?.props?.children ? exp.props.children.join("") : exp;
+    exp = exp?.props?.children?.join
+        ? exp.props.children.join("\n")
+        : exp?.props?.children
+            ? exp.props.children
+            : exp;
     return <MathComponent display={block} tex={String.raw`${exp}`} />;
 };
 

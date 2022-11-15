fe1cd42a62076b510e2c7adb18c3de34a5308135
Matilde Park <m@park.computer>
2022-11-15 15:30:49 -0600

    release: 0.6.2
    Adds MenuTray component.
    
    Fixes #21

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,6 +2,7 @@ export { default as BackgroundImage } from "./BackgroundImage";
 export { default as Container } from "./Container";
 export { default as Footer } from "./Footer";
 export { default as IntraNav } from "./IntraNav";
+export { default as MenuTray } from "./MenuTray";
 export { default as RenderTree } from "./RenderTree";
 export { default as Section } from "./Section";
 export { default as SingleColumn } from "./SingleColumn";
